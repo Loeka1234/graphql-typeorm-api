48bb37c8bac8f2c8db056fe1854e6b7bc01fad28
Loeka1234 <Loeka1234@users.noreply.github.com>
2020-09-01 21:26:31 +0200

    Added logout

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,17 +1,18 @@
 import {
-  Resolver,
-  Mutation,
-  Field,
-  Arg,
-  ObjectType,
-  Ctx,
-  Query,
-  FieldResolver,
-  Root,
+	Resolver,
+	Mutation,
+	Field,
+	Arg,
+	ObjectType,
+	Ctx,
+	Query,
+	FieldResolver,
+	Root,
 } from "type-graphql";
 import { User } from "../entities/User";
 import argon2 from "argon2";
 import { MyContext } from "src/types";
+import { COOKIE_NAME } from "../constants";
 
 // @InputType()
 // class RegisterInput {
@@ -27,138 +28,154 @@ import { MyContext } from "src/types";
 
 @ObjectType()
 class FieldError {
-  @Field()
-  field: string;
+	@Field()
+	field: string;
 
-  @Field()
-  message: string;
+	@Field()
+	message: string;
 }
 
 @ObjectType()
 class UserResponse {
-  @Field(() => FieldError, { nullable: true })
-  error?: FieldError;
+	@Field(() => FieldError, { nullable: true })
+	error?: FieldError;
 
-  @Field(() => User, { nullable: true })
-  user?: User;
+	@Field(() => User, { nullable: true })
+	user?: User;
 }
 
 @Resolver(User)
 export class UserResolver {
-  @FieldResolver()
-  email(@Root() user: User, @Ctx() { req }: MyContext) {
-    if (req.session.userId === user.id) return user.email;
-    else return "";
-  }
+	@FieldResolver()
+	email(@Root() user: User, @Ctx() { req }: MyContext) {
+		if (req.session.userId === user.id) return user.email;
+		else return "";
+	}
 
-  @Query(() => User, { nullable: true })
-  async me(@Ctx() { req }: MyContext) {
-    if (!req.session.userId) {
-      return null;
-    }
+	@Query(() => User, { nullable: true })
+	async me(@Ctx() { req }: MyContext) {
+		if (!req.session.userId) {
+			return null;
+		}
 
-    const user = await User.findOne({ id: req.session.userId });
-    return user;
-  }
+		const user = await User.findOne({ id: req.session.userId });
+		return user;
+	}
 
-  @Mutation(() => UserResponse)
-  async register(
-    @Arg("username") username: string,
-    @Arg("password") password: string,
-    @Arg("email") email: string,
-    @Ctx() { req }: MyContext
-  ): Promise<UserResponse> {
-    if (username.length < 5)
-      return {
-        error: {
-          field: "username",
-          message: "length must be greater than 4",
-        },
-      };
+	@Mutation(() => UserResponse)
+	async register(
+		@Arg("username") username: string,
+		@Arg("password") password: string,
+		@Arg("email") email: string,
+		@Ctx() { req }: MyContext
+	): Promise<UserResponse> {
+		if (username.length < 5)
+			return {
+				error: {
+					field: "username",
+					message: "length must be greater than 4",
+				},
+			};
 
-    if (!password.match(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/))
-      return {
-        error: {
-          field: "password",
-          message:
-            "must contain at least one letter, at least one number, and be longer than six charaters",
-        },
-      };
+		if (!password.match(/^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$/))
+			return {
+				error: {
+					field: "password",
+					message:
+						"must contain at least one letter, at least one number, and be longer than six charaters",
+				},
+			};
 
-    const usernameAlreadyExists = await User.findOne(
-      {
-        username: username.toLowerCase(),
-      },
-      { select: ["id"] }
-    );
+		const usernameAlreadyExists = await User.findOne(
+			{
+				username: username.toLowerCase(),
+			},
+			{ select: ["id"] }
+		);
 
-    if (usernameAlreadyExists)
-      return {
-        error: {
-          field: "username",
-          message: "user already exists",
-        },
-      };
+		if (usernameAlreadyExists)
+			return {
+				error: {
+					field: "username",
+					message: "user already exists",
+				},
+			};
 
-    const emailAlreadyExists = await User.findOne(
-      { email },
-      { select: ["id"] }
-    );
-    if (emailAlreadyExists)
-      return {
-        error: {
-          field: "email",
-          message: "email already has an account",
-        },
-      };
+		const emailAlreadyExists = await User.findOne(
+			{ email },
+			{ select: ["id"] }
+		);
+		if (emailAlreadyExists)
+			return {
+				error: {
+					field: "email",
+					message: "email already has an account",
+				},
+			};
 
-    const hashedPassword = await argon2.hash(password);
+		const hashedPassword = await argon2.hash(password);
 
-    const user = await User.create({
-      username: username.toLowerCase(),
-      email,
-      password: hashedPassword,
-    }).save();
+		const user = await User.create({
+			username: username.toLowerCase(),
+			email,
+			password: hashedPassword,
+		}).save();
 
-    req.session.userId = user.id;
+		req.session.userId = user.id;
 
-    return {
-      user,
-    };
-  }
+		return {
+			user,
+		};
+	}
 
-  @Mutation(() => UserResponse)
-  async login(
-    @Arg("username") username: string,
-    @Arg("password") password: string,
-    @Ctx() { req }: MyContext
-  ): Promise<UserResponse | undefined> {
-    const user = await User.findOne({
-      username: username.toLowerCase(),
-    });
+	@Mutation(() => UserResponse)
+	async login(
+		@Arg("username") username: string,
+		@Arg("password") password: string,
+		@Ctx() { req }: MyContext
+	): Promise<UserResponse | undefined> {
+		const user = await User.findOne({
+			username: username.toLowerCase(),
+		});
 
-    if (!user)
-      return {
-        error: {
-          field: "username",
-          message: "user doesn't exist",
-        },
-      };
+		if (!user)
+			return {
+				error: {
+					field: "username",
+					message: "user doesn't exist",
+				},
+			};
 
-    const valid = await argon2.verify(user.password, password);
+		const valid = await argon2.verify(user.password, password);
 
-    if (!valid)
-      return {
-        error: {
-          field: "password",
-          message: "incorrect password",
-        },
-      };
+		if (!valid)
+			return {
+				error: {
+					field: "password",
+					message: "incorrect password",
+				},
+			};
 
-    req.session.userId = user.id;
+		req.session.userId = user.id;
 
-    return {
-      user,
-    };
-  }
+		return {
+			user,
+		};
+	}
+
+	@Mutation(() => Boolean)
+	async logout(@Ctx() { req, res }: MyContext) {
+		return new Promise(resolve =>
+			req.session.destroy(err => {
+				res.clearCookie(COOKIE_NAME);
+				if (err) {
+					console.log(err);
+					resolve(false);
+					return;
+				}
+
+				resolve(true);
+			})
+		);
+	}
 }
