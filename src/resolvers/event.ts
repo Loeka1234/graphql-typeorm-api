370b543a3e3fe0667accfb423f9b633bb28d8a8e
Loeka1234 <Loeka1234@users.noreply.github.com>
2020-09-02 18:23:57 +0200

    Optimized events

--- src/resolvers/event.ts
+++ src/resolvers/event.ts
@@ -1,13 +1,13 @@
 import {
-  Resolver,
-  Mutation,
-  Arg,
-  Query,
-  Int,
-  Ctx,
-  Authorized,
-  ObjectType,
-  Field,
+	Resolver,
+	Mutation,
+	Arg,
+	Query,
+	Int,
+	Ctx,
+	Authorized,
+	ObjectType,
+	Field,
 } from "type-graphql";
 import { Event } from "../entities/Event";
 import { MyContext } from "src/types";
@@ -16,88 +16,90 @@ import { FieldError } from "../utils/FieldError";
 
 @ObjectType()
 class EventResponse {
-  @Field(() => FieldError, { nullable: true })
-  error?: FieldError;
+	@Field(() => FieldError, { nullable: true })
+	error?: FieldError;
 
-  @Field(() => Event, { nullable: true })
-  event?: Event;
+	@Field(() => Event, { nullable: true })
+	event?: Event;
 }
 
 @Resolver()
 export class EventResolver {
-  @Mutation(() => EventResponse)
-  @Authorized()
-  async createEvent(
-    @Arg("title") title: string,
-    @Arg("description", () => String, { nullable: true }) description: string,
-    @Ctx() { req }: MyContext
-  ): Promise<EventResponse> {
-    if (!title || title.length < 5)
-      return {
-        error: {
-          field: "title",
-          message: "title should have a minimul length of 4",
-        },
-      };
+	@Mutation(() => EventResponse)
+	@Authorized()
+	async createEvent(
+		@Arg("title") title: string,
+		@Arg("description", () => String, { nullable: true })
+		description: string,
+		@Ctx() { req }: MyContext
+	): Promise<EventResponse> {
+		if (!title || title.length < 5)
+			return {
+				error: {
+					field: "title",
+					message: "title should have a minimul length of 4",
+				},
+			};
 
-    const event = await Event.create({
-      title,
-      description,
-      creatorId: req.session.userId,
-    }).save();
-    
-    return {
-      event,
-    };
-  }
+		const event = await Event.create({
+			title,
+			description,
+			creatorId: req.session.userId,
+		}).save();
 
-  @Query(() => [Event])
-  @Authorized()
-  async events(@Ctx() { req }: MyContext): Promise<Event[]> {
-    const user = await User.findOne({ id: req.session.userId });
+		return {
+			event,
+		};
+	}
 
-    return Event.find({
-      where: { creatorId: user?.id },
-    });
-  }
+	@Query(() => [Event])
+	@Authorized()
+	async events(@Ctx() { req }: MyContext): Promise<Event[]> {
+		const user = await User.findOne({ id: req.session.userId });
 
-  @Query(() => Event, { nullable: true })
-  eventById(@Arg("id", () => Int) id: number): Promise<Event | undefined> {
-    return Event.findOne({ where: { id }, relations: ["creator"] });
-  }
+		return Event.find({
+			where: { creatorId: user?.id },
+		});
+	}
 
-  @Mutation(() => Event, { nullable: true })
-  @Authorized()
-  async updateEvent(
-    @Arg("id", () => Int) id: number,
-    @Arg("title", () => String, { nullable: true }) title: string,
-    @Arg("description", () => String, { nullable: true }) description: string,
-    @Ctx() { req }: MyContext
-  ) {
-    const event = await Event.findOne({
-      id,
-    });
-    if (!event || (!title && !description)) return null;
+	@Query(() => Event, { nullable: true })
+	eventById(@Arg("id", () => Int) id: number): Promise<Event | undefined> {
+		return Event.findOne({ where: { id }, relations: ["creator"] });
+	}
 
-    if (req.session.userId !== event.creatorId) return null;
+	@Mutation(() => Event, { nullable: true })
+	@Authorized()
+	async updateEvent(
+		@Arg("id", () => Int) id: number,
+		@Arg("title", () => String, { nullable: true }) title: string,
+		@Arg("description", () => String, { nullable: true })
+		description: string,
+		@Ctx() { req }: MyContext
+	) {
+		const event = await Event.findOne({
+			id,
+		});
+		if (!event || (!title && !description)) return null;
 
-    if (title) event.title = title;
-    if (description) event.description = description;
+		if (req.session.userId !== event.creatorId) return null;
 
-    const updatedEvent = await event.save();
-    return updatedEvent;
-  }
+		if (title) event.title = title;
+		if (description) event.description = description;
 
-  @Mutation(() => Boolean, { nullable: true })
-  async deleteEvent(@Arg("id") id: number, @Ctx() { req }: MyContext) {
-    const event = await Event.findOne({
-      where: { id },
-      relations: ["creator"],
-    });
+		const updatedEvent = await event.save();
+		return updatedEvent;
+	}
 
-    if (!event || req.session.userId !== event.creatorId) return false;
+	@Mutation(() => Boolean)
+	@Authorized()
+	async deleteEvent(@Arg("id", () => Int) id: number, @Ctx() { req }: MyContext) {
+		const event = await Event.findOne({
+			id,
+		});
 
-    await event.remove();
-    return true;
-  }
+		if (!event || req.session.userId !== event.creatorId) return false;
+
+		await event.remove();
+		return true;
+	}
 }
